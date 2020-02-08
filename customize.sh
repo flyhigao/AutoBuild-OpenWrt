9f23b4a31bbdd261bc74461f140a0cf873f3059c
flyhigao <47943621+flyhigao@users.noreply.github.com>
2020-02-08 21:10:06 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -12,7 +12,7 @@ sed -i 's/192.168.1.1/192.168.2.1/g' openwrt/package/base-files/files/bin/config
 sed -i 's/option redirect_https\t1/option redirect_https\t0/g' openwrt/package/network/services/uhttpd/files/uhttpd.config
 
 #gao3. use auth for ttyd in port 7681
-sed -i "s/option command.*/option command '\/bin\/login'/g" openwrt/staging_dir/target-x86_64_musl/root-x86/etc/config/ttyd
+sed -i "s/option command.*/option command '\/bin\/login'/g" openwrt/feeds/packages/utils/ttyd/files/ttyd.config
 
 #gao4. use auth for all console login
-sed -i "s/\/usr\/libexec\/login\.sh/\/bin\/login/g" ./target/linux/x86/base-files/etc/inittab
+sed -i "s/\/usr\/libexec\/login\.sh/\/bin\/login/g" openwrt/target/linux/x86/base-files/etc/inittab
