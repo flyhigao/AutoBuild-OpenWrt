51f3f865109302dea5bcb2df3dbad74840ef8267
flyhigao <47943621+flyhigao@users.noreply.github.com>
2020-07-02 20:45:12 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -10,6 +10,9 @@ sed -i 's/192.168.1.1/192.168.2.1/g' openwrt/package/base-files/files/bin/config
 
 #gao2. disable https redirect
 sed -i 's/option redirect_https\t1/option redirect_https\t0/g' openwrt/package/network/services/uhttpd/files/uhttpd.config
+if  ! grep 443 openwrt/package/network/services/uhttpd/files/uhttpd.config ;then
+        sed -i '/0.0.0.0:80/a\\tlist listen_https\t'0.0.0.0:443'' openwrt/package/network/services/uhttpd/files/uhttpd.config
+fi
 
 #gao3. use auth for ttyd in port 7681
 sed -i "s/option command.*/option command '\/bin\/login'/g" openwrt/feeds/packages/utils/ttyd/files/ttyd.config
